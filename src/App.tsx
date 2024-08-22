d70cb7ed313ca8cff361b64554f382d188f8fcfb
Brandon Liu <bdon@bdon.org>
2024-08-22 23:04:51 +0800

    update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,12 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
       </div>
       <h1>Vite + Solid</h1>
       <div class="card">
