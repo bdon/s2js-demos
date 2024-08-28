37a932998a1dfaebc256624186d076500439e228
Peter Johnson <insomnia@rcpt.at>
2024-08-28 11:42:20 +0200

    spherical rendering fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import { r1, s2, s1 } from "s2js";
 import { greatCircle } from "@turf/great-circle";
 import { flatten } from "@turf/flatten";
-import { polygonize } from "@turf/polygonize";
 import {
   TerraDraw,
   TerraDrawMapLibreGLAdapter,
@@ -16,20 +15,37 @@ import {
 } from "terra-draw";
 import {
   Feature,
-  Position,
   Polygon,
   LineString,
   FeatureCollection,
+  MultiLineString,
 } from "geojson";
 
 const initialUnion = (): s2.CellUnion => {
   const union = new s2.CellUnion();
-  union.push(s2.cellid.fromFace(0));
-  union.push(s2.cellid.fromFace(1));
+  // union.push(s2.cellid.fromFace(0));
+  // union.push(s2.cellid.fromFace(1));
   // union.push(s2.cellid.fromFace(2));
-  union.push(s2.cellid.fromFace(3));
-  union.push(s2.cellid.fromFace(4));
+  // union.push(s2.cellid.fromFace(3));
+  // union.push(s2.cellid.fromFace(4));
   // union.push(s2.cellid.fromFace(5));
+
+  // union.push(2985886552946638848n);
+  // union.push(3008404551083491328n);
+  // union.push(3098476543630901248n);
+  // union.push(7710162562058289152n);
+  // union.push(7854277750134145024n);
+  // union.push(8286623314361712640n);
+  // union.push(11957057010668666880n);
+  // union.push(11979575008805519360n);
+  // union.push(12087661399862411264n);
+  // union.push(12393906174523604992n);
+  // union.push(5719853001737240576n);
+  // union.push(5476377146882523136n);
+  // union.push(4899916394579099648n);
+  // union.push(s2.cellid.fromToken("5b"));
+  // union.push(s2.cellid.fromToken("a4"));
+  // union.push(s2.cellid.fromToken("4c"));
   return union;
 };
 
@@ -69,7 +85,7 @@ const getCovering = (
   );
 };
 
-const fixAntimeridianCrossings = (arcLines: FeatureCollection<LineString>) => {
+const wrapAntimeridianCrossings = (arcLines: FeatureCollection<LineString>) => {
   if (arcLines.features.length <= 4) return;
 
   const antimeridianCrossings: boolean[] = [];
@@ -90,11 +106,53 @@ const fixAntimeridianCrossings = (arcLines: FeatureCollection<LineString>) => {
     last = f.geometry.coordinates.at(-1)!;
   });
 
-  // fix lats
+  // wrap lats
   arcLines.features.forEach((f, fi) => {
     if (!antimeridianCrossings[fi]) return;
     f.geometry.coordinates.forEach((v) => (v[0] += 360));
   });
+};
+
+const fixPolarFaces = (arcs: Feature<LineString | MultiLineString>[]) => {
+  arcs.forEach((arc: Feature<LineString | MultiLineString>) => {
+    if (arc.geometry.type === "MultiLineString") {
+      const A = arc.geometry.coordinates[0].at(-1)!;
+      const B = arc.geometry.coordinates[1].at(0)!;
+
+      // sanity checks
+      if (!A || Math.abs(A[0]) !== 180 || !B || Math.abs(B[0]) !== 180) {
+        return;
+      }
+
+      // the target polar latitude
+      const E = Math.sign(A[0]) * 90;
+
+      // draw a line to the pole, across, and back again
+      arc.geometry.coordinates = [
+        arc.geometry.coordinates[0],
+        [A, [A[0], E], [B[0], E], B],
+        arc.geometry.coordinates[1],
+      ];
+    }
+  });
+};
+
+const fixPoles = (points: number[][]) => {
+  points.forEach((p, i) => {
+    if (Math.abs(p[1]) === 90) p[0] = points.at(i - 1)![0];
+  });
+};
+
+const fixFalseAntimeridianCrossings = (points: number[][]) => {
+  const exterior = points.filter((p) => Math.abs(p[0]) === 180);
+  if (exterior.length !== 2) return;
+  if (Math.sign(exterior[0][0]) !== Math.sign(exterior[1][0])) return;
+
+  const interior = points.filter((p) => Math.abs(p[0]) !== 180);
+  if (interior.length !== 2) return;
+  if (Math.sign(interior[0][0]) !== Math.sign(interior[1][0])) return;
+
+  exterior.forEach((p) => (p[0] = Math.sign(interior[0][0]) * 180));
 };
 
 const getCellVisualization = (union: s2.CellUnion): FeatureCollection => {
@@ -106,10 +164,29 @@ const getCellVisualization = (union: s2.CellUnion): FeatureCollection => {
     const v2 = s2.LatLng.fromPoint(cell.vertex(2));
     const v3 = s2.LatLng.fromPoint(cell.vertex(3));
 
-    const p0 = [degrees(v0.lng), degrees(v0.lat)];
-    const p1 = [degrees(v1.lng), degrees(v1.lat)];
-    const p2 = [degrees(v2.lng), degrees(v2.lat)];
-    const p3 = [degrees(v3.lng), degrees(v3.lat)];
+    const p0 = [degrees(v0.lng) || 0, degrees(v0.lat) || 0];
+    const p1 = [degrees(v1.lng) || 0, degrees(v1.lat) || 0];
+    const p2 = [degrees(v2.lng) || 0, degrees(v2.lat) || 0];
+    const p3 = [degrees(v3.lng) || 0, degrees(v3.lat) || 0];
+
+    fixPoles([p0, p1, p2, p3]);
+    fixFalseAntimeridianCrossings([p0, p1, p2, p3]);
+
+    // console.log(p0, p1, p2, p3);
+
+    // if ([p0, p1, p2, p3].filter((p) => !p[0]).length % 2 !== 0) {
+    //   console.log("potential line drawing issue", cellid);
+    //   console.log(p0, p1, p2, p3);
+    // }
+
+    // if (
+    //   [p0, p1, p2, p3].filter(
+    //     (p) => Math.abs(p[0]) === 180 && Math.abs(p[1]) === 90,
+    //   ).length
+    // ) {
+    //   console.log("potential pole issue", cellid);
+    //   console.log(p0, p1, p2, p3);
+    // }
 
     const level = cell.level;
     const npoints = 20 + (30 - level) * 3;
@@ -117,6 +194,29 @@ const getCellVisualization = (union: s2.CellUnion): FeatureCollection => {
     const arc1 = greatCircle(p1, p2, { npoints });
     const arc2 = greatCircle(p2, p3, { npoints });
     const arc3 = greatCircle(p3, p0, { npoints });
+
+    const removeRedundantArcs = (
+      arcs: Feature<LineString | MultiLineString>[],
+    ) => {
+      arcs.forEach((arc: Feature<LineString | MultiLineString>) => {
+        if (arc.geometry.type !== "MultiLineString") return;
+        if (arc.geometry.coordinates.length !== 2) return;
+        arc.geometry.coordinates = arc.geometry.coordinates.filter((ls) => {
+          if (ls.length !== 2) return true;
+          if (ls[0][0] === ls[1][0] && ls[0][1] === ls[1][1]) return false;
+          return true;
+        });
+      });
+    };
+
+    removeRedundantArcs([arc0, arc1, arc2, arc3]);
+
+    // console.log(arc0, arc1, arc2, arc3);
+    // planar hacks for the two polar faces
+    const POLAR_FACES = [s2.cellid.fromFace(2), s2.cellid.fromFace(5)];
+    if (POLAR_FACES.includes(cellid)) {
+      fixPolarFaces([arc0, arc1, arc2, arc3]);
+    }
 
     const arcLines: FeatureCollection<LineString> = {
       type: "FeatureCollection",
@@ -128,22 +228,14 @@ const getCellVisualization = (union: s2.CellUnion): FeatureCollection => {
       ],
     };
 
-    fixAntimeridianCrossings(arcLines);
-
-    // const poly = polygonize(arcLines).features[0];
-    // poly.properties = { level: cell.level };
-    // return poly;
+    // fixes;
+    if (!POLAR_FACES.includes(cellid)) {
+      wrapAntimeridianCrossings(arcLines);
+    }
 
     const coordinates = arcLines.features
       .map((f) => f.geometry.coordinates)
       .flat(1);
-
-    // const coordinates = [
-    //   ...arc0.features.map((f) => f.geometry.coordinates).flat(1),
-    //   ...arc1.features.map((f) => f.geometry.coordinates).flat(1),
-    //   ...arc2.features.map((f) => f.geometry.coordinates).flat(1),
-    //   ...arc3.features.map((f) => f.geometry.coordinates).flat(1),
-    // ] as Position[];
 
     return {
       type: "Feature",
@@ -153,6 +245,7 @@ const getCellVisualization = (union: s2.CellUnion): FeatureCollection => {
       },
       properties: {
         level: cell.level,
+        token: s2.cellid.toToken(cell.id),
       },
     };
   });
